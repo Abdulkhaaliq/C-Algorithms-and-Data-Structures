77d5bdfb468b752c0312aeed03dd3889acdbddf7
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2021-07-19 15:22:01 +0200

    Added

---
+++ Algorithms c-Sharp/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Provide a number:");
+
+            int result = Int32.Parse(Console.ReadLine());
+
+            int[] arr = { 1, 25, 64, 89, 4, 12, 32, 28, 100, 230 };
+
+            BinarySearch(arr, result);
+        }
+
+        private static int BinarySearch(int[] arr, int searchedTerm)
+        {
+            int begin = 0;
+            int end = arr.Length - 1;
+            while(begin <= end)
+            {
+                int mid = (begin + end) / 2;
+                if (searchedTerm == arr[mid])
+                {
+                    Console.WriteLine($"{searchedTerm} was found after {end} times");
+                    return mid;
+                }
+                if(searchedTerm < arr[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    begin = mid + 1;
+                }
+            }
+            Console.WriteLine($"{searchedTerm} was not found");
+            return -1;
+        }
+    }
+}
---
+++ Algorithms c-Sharp/LinearSearch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 25, 64, 89, 4, 12, 32, 28, 100, 230};
+
+            Console.WriteLine("Enter a number");
+
+            string result = Console.ReadLine();
+
+            //Converts string to int
+            int number = int.Parse(result);
+
+            LinearSearchInArray(arr, number);
+
+            Console.WriteLine("");
+            // for all recuression lovers out there
+            var recurssionResult = RecursionLinearSearch(arr, 0, number);
+            if (recurssionResult < 0)
+                Console.WriteLine($"{number} Was not found");
+            else
+                Console.WriteLine($"{number} was found after {recurssionResult} times");
+        }
+
+        private static int RecursionLinearSearch(int[] arr, int index, int searchedTerm)
+        {
+            if (index > arr.Length)
+                return -1;
+            if (arr[index] == searchedTerm)
+                return index;
+            return RecursionLinearSearch(arr, index + 1, searchedTerm);
+        }
+
+        public static int LinearSearchInArray(int[] arr, int number)
+        {
+            //iterates the length of array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //if iteration is equal to the value passed
+                if(arr[i] == number)
+                {
+                    Console.WriteLine($"It was found after {i} iterations");
+                    return i;
+                }
+            }
+            //if value was not found
+            Console.WriteLine("It was not found");
+            return -1;
+        }
+    }
+}
