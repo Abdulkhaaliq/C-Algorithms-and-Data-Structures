ffacc16132a01b93e1cbd16931742d30cfbf0979
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2021-07-22 15:00:46 +0200

    Added

---
+++ Algorithms c-Sharp/QueueDataStructure/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace QueueDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(3);
+            DisplayHeader();
+            char option = Console.ReadLine()[0];
+            while (option != 'Q') 
+            {
+                switch (option)
+                {
+                    case 'I':
+                        Console.Write("Enter Item: ");
+                        try
+                        {
+                            queue.Insert(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+
+                    case 'P':
+                        try
+                        {
+                            string result = (string)queue.Peek();
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+                    case 'R':
+                        try
+                        {
+                            queue.Remove();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+                    case 'S':
+                        Console.WriteLine(queue.ToString());
+                        break;
+                    case 'X':
+                        queue.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                
+                DisplayHeader();
+                option = Console.ReadLine()[0];
+            }
+        }
+
+        public static void DisplayHeader()
+        {
+            Console.WriteLine("Enter one of the following: ");
+            Console.WriteLine("I - insert");
+            Console.WriteLine("P - peek");
+            Console.WriteLine("R - remove");
+            Console.WriteLine("S - show queue");
+            Console.WriteLine("X - clear queue");
+            Console.WriteLine("Q - quit");
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ Algorithms c-Sharp/QueueDataStructure/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueDataStructure
+{
+    public class Queue
+    {
+        public int front;
+        public int rear = -1;
+        public int numElements;
+        object[] element;
+        int maxSize;
+
+        public Queue(int data)
+        {
+            element = new object[data];
+            maxSize = data;
+        }
+
+        public bool Empty { get { return numElements == 0; } }
+        public bool Full { get { return numElements == maxSize; } }
+        public int Size { get { return numElements; } }
+
+
+
+        public void Insert(object item)
+        {
+            if(Full) throw new Exception("Queue is full");
+            rear = (rear + 1) % maxSize;
+            element[rear] = item;
+            numElements++;
+        }
+
+        public object Peek()
+        {
+            if(Empty) throw new Exception("Queue is empty");
+            return element[front];
+        }
+
+        public void Remove()
+        {
+            if (Empty) throw new Exception("Queue is empty");
+            element[front] = null;
+            front = (front - 1) % maxSize;
+            numElements--;
+        }
+
+        public void Clear()
+        {
+            while (!Empty)
+            {
+                Remove();
+            }
+        }
+
+        public override string ToString()
+        {
+            string ans = "";
+            int traveler = front;
+            for(int i = 0; i < numElements; i++)
+            {
+                ans = ans + " " + element[traveler].ToString();
+                traveler = (traveler + 1) % maxSize;
+            }
+            return ans;
+        }
+    }
+
+}
